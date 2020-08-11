08186001ca8f286e4a92bb0287a9f2e76df173df
Emily Chen <wanchichen88@gmail.com>
2020-08-11 21:18:00 +0800

    新增、編輯功能

--- src/views/dashboard/Coupons.vue
+++ src/views/dashboard/Coupons.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="coupons">
-    <h2 class="pt-3 text-center">優惠券</h2>
-    <div class="d-flex justify-content-end">
+      <Loading :active.sync="isLoading" />
+      <h2 class="pt-3 text-center">優惠券</h2>
+      <div class="d-flex justify-content-end">
         <div type="button" class="btn btn-primary mb-3"
-        @click="open">
+        @click="openCouponModal('create')">
         建立優惠券
         </div>
-    </div>
+      </div>
         <!-- 表格 -->
         <table class="table product">
             <thead class="font-weight-bold">
@@ -21,7 +22,7 @@
             </thead>
             <tbody v-if="coupons.length">
                 <tr v-for="(item, key) in coupons" :key="key">
-                    <td v-for="(num, i) in 25" :key="i">{{ i }}</td>
+                    <td >{{ key+1 }}</td>
                     <td>{{ item.title}}</td>
                     <td>{{ item.percent }}</td>
                     <td>{{ item.deadline.datetime }}</td>
@@ -60,23 +61,35 @@
                         <div class="modal-body">
                           <div class="form-group">
                               <label for="textInputTitle" class="mb-1">標題</label>
-                              <input type="text" class="form-control" id="textInputTitle">
+                              <input type="text" class="form-control"
+                                id="textInputTitle" v-model="tempCoupon.title">
                           </div>
                           <div class="form-group">
                               <label for="textInputCode" class="mb-1">優惠碼</label>
-                              <input type="text" class="form-control" id="textInputCode">
+                              <input type="text" class="form-control"
+                              id="textInputCode" v-model="tempCoupon.code">
                           </div>
                           <div class="form-group">
                               <label for="expiryDate">到期日</label>
-                              <input type="date" class="form-control" id="expiryDate" >
+                              <input type="date" class="form-control"
+                              id="expiryDate" v-model="due_date" >
                           </div>
                           <div class="form-group">
                               <label for="expiryTime">到期時間</label>
-                              <input type="time" class="form-control" id="expiryTime" >
+                              <input type="time" class="form-control"
+                              id="expiryTime" v-model="due_time">
                           </div>
                           <div class="form-group">
                               <label for="inputPercent">折扣百分比</label>
-                              <input type="num" class="form-control" id="inputPercent" >
+                              <input type="num" class="form-control"
+                              id="inputPercent"  v-model="tempCoupon.percent">
+                          </div>
+                          <div class="form-group form-check">
+                              <input type="checkbox" class="form-check-input"
+                              id="checkEnabled"
+                              v-model="tempCoupon.enabled">
+                              <label class="form-check-label"
+                              for="checkEnabled">是否啟用</label>
                           </div>
 
                         </div>
@@ -85,7 +98,7 @@
                             data-dismiss="modal">取消</button>
                             <button type="button" class="btn btn-primary"
                             @click="updateCoupon">
-                              {{ status === 'created' ? '新增優惠卷' : '更新優惠券' }}
+                              {{ status === 'create' ? '新增優惠卷' : '更新優惠券' }}
                             </button>
                         </div>
                     </div>
@@ -104,7 +117,7 @@
                       </button>
                   </div>
                   <div class="modal-body">
-                      是否刪除 優惠券名稱(刪除後將無法恢復)。
+                      是否刪除 {{ tempCoupon.title }} (刪除後將無法恢復)。
                   </div>
                   <div class="modal-footer">
                       <button type="button" class="btn btn-outline-secondary"
@@ -128,11 +141,7 @@ export default {
       coupons: {
       },
       tempCoupon: {
-        title: '',
-        enabled: false,
-        percent: 10,
         deadline_at: 0,
-        code: '',
       },
       due_date: '',
       due_time: '',
@@ -161,22 +170,23 @@ export default {
       });
     },
     openCouponModal(action, item) {
+      this.status = action;
       switch (action) {
-        case 'create':
-          this.tempCoupon = {
-            title: '',
-            code: '',
-            percent: 0,
-            enabled: false,
-            deadline_at: 0,
-          };
+        case 'create':// 新增
+          this.tempCoupon = {};
           $('#couponModal').modal('show');
           break;
-        case 'edit': // 編輯
+        case 'edit': { // 編輯
+          this.tempCoupon = { ...item };// 淺拷貝
+          // 用 split 來處理時間
+          const dedlineAt = this.tempCoupon.deadline.datetime.split(' ');
+          [this.due_date, this.due_time] = dedlineAt; // 日期-用解構來給值
+
           $('#couponModal').modal('show');
           break;
+        }
         case 'delete':// 刪除
-          this.tempCoupon = { ...item };
+          this.tempCoupon = { ...item };// 淺拷貝
           $('#delCouponModal').modal('show');
           break;
         default:
@@ -184,38 +194,58 @@ export default {
       }
     },
     updateCoupon() {
+      // 用status狀態來判斷是新增(post)還是編輯(patch)
       // POST api/{uuid}/admin/ec/coupon
+      // PATCH api/{uuid}/admin/ec/coupon/{id}
+      this.isLoading = true;
       const url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/coupon`;
-      this.$http.post(url).then(() => {
-        this.getCoupons();
-        $('#couponModal').modal('hide');
-        this.isLoading = false;
-      });
+      const editUrl = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/coupon/${this.tempCoupon.id}`;
+      // deadline_at 要符合 Y-m-d H:i:s 的格式例如：「2020-06-16 09:31:18」
+      this.tempCoupon.deadline_at = `${this.due_date} ${this.due_time}`;
+      if (this.status === 'create') {
+        this.$http.post(url, this.tempCoupon).then(() => {
+          $('#couponModal').modal('hide');
+          this.$bus.$emit('message:push',
+            '完成新增優惠券 ',
+            'success');
+          this.getCoupons();
+          this.isLoading = false;
+        });
+      } else {
+        this.$http.patch(editUrl, this.tempCoupon)
+          .then(() => {
+            $('#couponModal').modal('hide');
+            this.$bus.$emit('message:push',
+              '完成編輯優惠券資訊 ',
+              'success');
+            this.getCoupons();
+            this.isLoading = false;
+          });
+      }
     },
     deleteCoupon() {
       this.isLoading = true;
       // DELETE api/{uuid}/admin/ec/coupon/{id}
-      const url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/product/${this.tempCoupon.id}`;
+      const url = `${process.env.VUE_APP_APIPATH}${process.env.VUE_APP_UUID}/admin/ec/coupon/${this.tempCoupon.id}`;
 
       this.$http.delete(url).then(() => {
-        this.getCoupons();
-        this.isLoading = false;
+        $('#deleteCouponModal').modal('hide');
         this.$bus.$emit('message:push',
           '刪除優惠券',
           'success');
-        $('#deleteCouponModal').modal('hide');
+        this.getCoupons();
+        this.isLoading = false;
       }).catch((error) => {
         // eslint-disable-next-line no-console
-        console.log(error);
+        console.log(error.message);
+        $('#deleteCouponModal').modal('hide');
         this.$bus.$emit('message:push',
           '刪除失敗，請找工程師XD',
           'warning');
         this.isLoading = false;
       });
     },
-    open() {
-      $('#couponModal').modal('show');
-    },
+
   },
 };
 
